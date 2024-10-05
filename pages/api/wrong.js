896e539e5e29839370eecac4e349e130d52adb61
Andres Giudice <giudiceandres@gmail.com>
2024-10-05 12:25:32 -0300

    third commit

--- pages/api/wrong.js
+++ pages/api/wrong.js
@@ -1,8 +1,16 @@
-import { connectToDatabase } from "../../lib/connectToDatabase";
+import { MongoClient} from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const options = {}; 
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
 
 export default async function handler(request, response) {
     try{
-        const {mongoClient} = await connectToDatabase();
+        const mongoClient = await (new MongoClient(uri, options)).connect();
+        console.log("Just Connected");
         const db = mongoClient.db('sample_mflix');
         const collection = db.collection('movies');    
         const results = await collection
@@ -11,7 +19,7 @@ export default async function handler(request, response) {
            title:1,
            _id: 0
         })
-        .limit(8)
+        .limit(4)
         .toArray();
         response.status(200).json(results);
     }catch(e){
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Inter } from "next/font/google";
+import client from "@/lib/mongodb";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
-
-type BagHandles = {
-  kpBagHandles: any[];
-  wpBagHandles: any[];
-};
+import { useEffect, useState } from "react";
 
 type ConnectionStatus = {
   isConnected: boolean;
-  bagHandles: BagHandles;
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,60 +15,89 @@ export const getServerSideProps: GetServerSideProps<
   ConnectionStatus
 > = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/bags");
-    const bagHandles = await res.json();
+    await client.connect(); // `await client.connect()` will use the default database passed in the MONGODB_URI
     return {
-      props: { isConnected: true, bagHandles },
+      props: { isConnected: true },
     };
   } catch (e) {
     console.error(e);
     return {
-      props: { isConnected: false, bagHandles: { kpBagHandles: [], wpBagHandles: [] } },
+      props: { isConnected: false },
     };
   }
 };
 
-export default function Home({
-  isConnected,
-  bagHandles,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+type Movie = {
+  title: string;
+};
+
+export default function Home({ isConnected }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const results = await fetch("/api/list");
+      const resultsJson = await results.json();
+      setMovies(resultsJson);
+    })();
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <div className="grid">
+        {movies.map((movie, index) => (
+          <div className="card" key={index}>
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
+      </div>
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Pages Router: Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.tsx</code>
         </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
       </div>
 
       <div className="flex flex-col place-items-center gap-12">
-        {isConnected ? (
-          <h2 className="text-lg text-green-500">
-            You are connected to MongoDB!
-          </h2>
-        ) : (
-          <h2 className="text-lg text-red-500">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-            for instructions.
-          </h2>
-        )}
-        <div>
-          <h3 className="text-xl font-bold">KP Bag Handles</h3>
-          <ul>
-            {bagHandles.kpBagHandles.map((handle, index) => (
-              <li key={index}>{JSON.stringify(handle)}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3 className="text-xl font-bold">WP Bag Handles</h3>
-          <ul>
-            {bagHandles.wpBagHandles.map((handle, index) => (
-              <li key={index}>{JSON.stringify(handle)}</li>
-            ))}
-          </ul>
+        <div className="relative flex place-items-center gap-6 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+          {" + "}
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] saturate-0 brightness-0 dark:saturate-100 dark:brightness-100"
+            src="/mongodb.svg"
+            alt="MongoDB Logo"
+            width={180}
+            height={37}
+            priority
+          />
         </div>
       </div>
     </main>
