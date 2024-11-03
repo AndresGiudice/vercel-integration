8b7d1b7c880413c432b9d9c6d70fadd0b95a3722
Andres Giudice <giudiceandres@gmail.com>
2024-11-03 17:46:16 -0300

    mobile menu

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -70,6 +70,13 @@ export default function Example() {
     placeOrder(cart, totalAmount, clearCart);
   };
 
+  const handleMobileMenuToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+    if (showCartDetails) {
+      setShowCartDetails(false);
+    }
+  };
+
   return (
     <header className="fixed top-0 left-0 right-0 bg-[#A6CE39] z-50">
       <nav aria-label="Global" className="mx-auto flex items-center justify-between p-2 lg:px-8">
@@ -92,11 +99,15 @@ export default function Example() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            onClick={() => setMobileMenuOpen(true)}
+            onClick={handleMobileMenuToggle}
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            {mobileMenuOpen ? (
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+            ) : (
+              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            )}
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -223,29 +234,8 @@ export default function Example() {
       )}
       <Dialog open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.0">
-              <span className="sr-only">Your Company</span>
-              <img alt="" src="evacor-logo.png" className="h-6 w-auto" />
-            </Link>
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900 mr-6"
-              onClick={() => setShowCartDetails(!showCartDetails)}
-            >
-              <i className="fas fa-shopping-cart cart-icon text-xl"></i>
-              {totalItems > 0 && <span className="ml-2">{totalItems}</span>}
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 mt-16">
+         
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
