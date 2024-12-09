0178ad02ae1ff62676e2dcd407cafd26a350ea99
Andres Giudice <giudiceandres@gmail.com>
2024-12-09 15:24:47 -0300

    env added

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -40,7 +40,7 @@ export default async function handler(request, response) {
     }
   } else if (request.method === 'GET') {
     try {
-      const directoryPath = path.join(process.cwd(), './pages/listas');
+      const directoryPath = path.join(process.cwd(), process.env.LISTAS_DIRECTORY);
       console.log(`Checking directory: ${directoryPath}`);
       if (!fs.existsSync(directoryPath)) {
         throw new Error(`Directory not found: ${directoryPath}`);
