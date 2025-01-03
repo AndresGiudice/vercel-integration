d58b0d5c51e502b6155846ad2c04e0486b6f2812
Andres Giudice <giudiceandres@gmail.com>
2025-01-03 19:05:24 -0300

    sending order button

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -73,6 +73,7 @@ function Example() {
   const [priceList, setPriceList] = useState<string | null>(null);
   const { user, logout, setUser } = useUser(); // Ensure setUser is defined
   const [showUserDetails, setShowUserDetails] = useState(false); // Add this line
+  const [isPlacingOrder, setIsPlacingOrder] = useState(false); // Add this line
 
   const handleLogout = () => {
     logout();
@@ -115,8 +116,10 @@ function Example() {
 
   const totalItems = Object.values(cart).reduce((acc, { quantity }) => acc + quantity, 0);
 
-  const handlePlaceOrder = () => {
-    placeOrder(cart, totalAmount, clearCart);
+  const handlePlaceOrder = async () => {
+    setIsPlacingOrder(true); // Add this line
+    await placeOrder(cart, totalAmount, clearCart);
+    setIsPlacingOrder(false); // Add this line
   };
 
   const handleMobileMenuToggle = () => {
@@ -306,11 +309,21 @@ function Example() {
                 Vaciar Carrito
               </button>
               <button
-                onClick={handlePlaceOrder}
-                className="bg-green-500 text-white px-4 py-2 rounded-lg w-full sm:w-auto"
-
+                onClick={isPlacingOrder ? () => {} : handlePlaceOrder} // Disable button when placing order
+                className={`bg-green-500 text-white px-4 py-2 rounded-lg w-full sm:w-auto ${isPlacingOrder ? 'opacity-50 cursor-not-allowed' : ''}`} // Add styles for disabled state
+                disabled={isPlacingOrder} // Disable button when placing order
               >
-                Realizar Pedido
+                {isPlacingOrder ? (
+                  <div className="flex items-center justify-center w-full">
+                    Enviando Pedido
+                    <svg className="animate-spin h-5 w-5 ml-2" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                  </div>
+                ) : (
+                  'Realizar Pedido'
+                )}
               </button>
             </div>
           </div>
