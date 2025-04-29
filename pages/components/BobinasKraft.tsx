b0f285b93eba8001aabe3762c77dccdac476917c
AndresGiudice <andresgiudice94@gmail.com>
2025-04-29 18:47:02 -0300

    BobinasKraft calculateFinalPrice

--- pages/components/BobinasKraft.tsx
+++ pages/components/BobinasKraft.tsx
@@ -254,49 +254,7 @@ const BagCard: React.FC<BagCardProps & { folderName: keyof Bag | "lista2" | "lis
             <p className="text-gray-700 text-lg">
               Precio x und. : 
               <span className="font-bold">
-                {(() => {
-                  let finalPrice = 0;
-
-                  if (folderName === 'lista2') {
-                    finalPrice = (bag.list2 / 1.105);
-                  } else if (folderName === 'lista2-10') {
-                    finalPrice = (bag.list2 * 0.9) / 1.105;
-                  } else if (folderName === 'lista2-10-2') {
-                    finalPrice = (bag.list2 * 0.8802) / 1.105;
-                  } else if (folderName === 'lista2-final') {
-                    finalPrice = (bag.list2);
-                  } else if (folderName === 'lista2-10-final') {
-                    finalPrice = bag.list2 * 0.9;
-                  } else if (folderName === 'lista2-10-2-final') {
-                    finalPrice = bag.list2 * 0.8802;
-                  } else if (folderName === 'lista3') {
-                    finalPrice = bag.list3 / 1.105;
-                  } else if (folderName === 'lista3-10') {
-                    finalPrice = (bag.list3 * 0.9) / 1.105;
-                  } else if (folderName === 'lista3-10-2') {
-                    finalPrice = (bag.list3 * 0.8802) / 1.105;
-                  } else if (folderName === 'lista3-final') {
-                    finalPrice = (bag.list3);
-                  } else if (folderName === 'lista3-10-final') {
-                    finalPrice = bag.list3 * 0.9;
-                  } else if (folderName === 'lista3-10-2-final') {
-                    finalPrice = bag.list3 * 0.8802;
-                  } else if (folderName === 'lista4') {
-                    finalPrice = bag.list4 / 1.105;
-                  } else if (folderName === 'lista4-10') {
-                    finalPrice = (bag.list4 * 0.9) / 1.105;
-                  } else if (folderName === 'lista4-10-2') {
-                    finalPrice = (bag.list4 * 0.8802) / 1.105;
-                  } else if (folderName === 'lista4-final') {
-                    finalPrice = bag.list4; 
-                  } else if (folderName === 'lista4-10-final') {
-                    finalPrice = bag.list4 * 0.9;
-                  } else if (folderName === 'lista4-10-2-final') {
-                    finalPrice = bag.list4 * 0.8802;
-                  }
-
-                  return `$${Math.round(finalPrice)}`;
-                })()}
+                 {calculateFinalPrice(folderName, bag)}
               </span>
             </p>
           </div>
